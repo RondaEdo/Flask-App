a5b69ec3a6a667851827b83fd3d448d5e802ce76
Ronda <johnronda0527@gmail.com>
2026-03-13 19:25:14 +0800

    created login form

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return "<h1>Hello World</h1>"
+    if request.method == 'GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username == 'edo' and password == 'hakdog':
+            return 'Success'
+        else:
+            return 'Failure'
 
 
 if __name__ == '__main__':
